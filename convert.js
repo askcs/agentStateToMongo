782368aa840f8d05cac99b68930253a9ead3f764
root <sshetty@ask-cs.com>
2015-10-12 01:43:38 +0200

    fix: issue if . exists in the field key

--- convert.js
+++ convert.js
@@ -28,6 +28,8 @@ mongo.connect("mongodb://" + dbParams.host + ":" + dbParams.port + "/" + dbParam
 		process.exit();
 	}).error(function(err) {
 		console.log(err);
+		database.close();
+		process.exit();
 	});
 });
 
@@ -35,22 +37,19 @@ mongo.connect("mongodb://" + dbParams.host + ":" + dbParams.port + "/" + dbParam
 var convertAll = function(db) {
 
 	return new Promise(function(resolve, reject) {
-		convertFilesInDir(db, agentDbCollection, eveAgentsPath).then(function(promisedResult) {
-			if (promisedResult.count) {
-				console.log("Following agent files are inserted: " + JSON.stringify(promisedResult.result));
-				console.log("Total agent data saved in mongoDB: " + promisedResult.count);
-			}
-		}).error(function(err) {
-			console.log(err);
-			reject(err);
-		});
 
-		convertFilesInDir(db, schedulerDbCollection, eveSchedulerPath).then(function(promisedResult) {
-			if (promisedResult.count) {
+		convertFilesInDir(db, agentDbCollection, eveAgentsPath).then(function(promisedResult) {
+			console.log("Following agent files are inserted: " + JSON.stringify(promisedResult.result));
+			console.log("Total agent data saved in mongoDB: " + promisedResult.count);
+
+			convertFilesInDir(db, schedulerDbCollection, eveSchedulerPath).then(function(promisedResult) {
 				console.log("Following scheduler files are inserted: " + JSON.stringify(promisedResult.result));
 				console.log("Total scheduler data saved in mongoDB: " + promisedResult.count);
 				resolve(promisedResult);
-			}
+			}).error(function(err) {
+				console.log(err);
+				reject(err);
+			});
 		}).error(function(err) {
 			console.log(err);
 			reject(err);
@@ -67,53 +66,59 @@ var convertFilesInDir = function(db, collection, dirPath) {
 			}
 			var count = 0;
 			var allFiles = [];
-			files.forEach(function(file) {
-				fs.readFile(dirPath + "/" + file, 'utf-8', function(err, json) {
-					if (err) {
-						reject(err);
-					} else {
-						var agentMongoData = {};
-						//initialize the agentMongoData
-						agentMongoData["timestamp"] = new Date().getTime();
-						agentMongoData["_id"] = file;
-						dbParams["id"] = file;
-						dbParams["collection"] = collection;
-						agentMongoData["myParams"] = dbParams;
-						if (json.trim().length == 0) {
-							json = "{}";
-						}
-						agentMongoData["properties"] = JSON.parse(json);
-						//write agentMongoData to db
-						insertToMongo(db, collection, agentMongoData).then(function(result) {
-							allFiles[count] = file;
-							if (++count == files.length) {
-								promisedResult["count"] = count;
-								promisedResult["result"] = allFiles;
-								resolve(promisedResult);
+			if (files.length == 0) {
+				promisedResult["count"] = count;
+				promisedResult["result"] = allFiles;
+				resolve(promisedResult);
+			} else {
+				files.forEach(function(file) {
+					fs.readFile(dirPath + "/" + file, 'utf-8', function(err, json) {
+						if (err) {
+							reject(err);
+						} else {
+							var agentMongoData = {};
+							//initialize the agentMongoData
+							agentMongoData["timestamp"] = new Date().getTime();
+							agentMongoData["_id"] = file;
+							dbParams["id"] = file;
+							dbParams["collection"] = collection;
+							agentMongoData["myParams"] = dbParams;
+							if (json.trim().length == 0) {
+								json = "{}";
 							}
-						}).error(function(err) {
-							//check if the json entity is returned back
-							if (err._id) {
-								insertToMongo(db, collection, err).then(function(result) {
-									console.log('Succesfully updated...');
-									allFiles[count] = file;
-									if (++count == files.length) {
-										promisedResult["count"] = count;
-										promisedResult["result"] = allFiles;
-										resolve(promisedResult);
-									}
-								}).error(function(err) {
+							agentMongoData["properties"] = JSON.parse(json);
+							//write agentMongoData to db
+							insertToMongo(db, collection, agentMongoData).then(function(result) {
+								allFiles[count] = file;
+								if (++count == files.length) {
+									promisedResult["count"] = count;
+									promisedResult["result"] = allFiles;
+									resolve(promisedResult);
+								}
+							}).error(function(err) {
+								//check if the json entity is returned back
+								if (err._id) {
+									insertToMongo(db, collection, err).then(function(result) {
+										console.log('Succesfully updated...');
+										allFiles[count] = file;
+										if (++count == files.length) {
+											promisedResult["count"] = count;
+											promisedResult["result"] = allFiles;
+											resolve(promisedResult);
+										}
+									}).error(function(err) {
+										console.log('Something really went wrong! Error: ' + JSON.stringify(err));
+										reject(err);
+									})
+								} else {
 									console.log('Something really went wrong! Error: ' + JSON.stringify(err));
 									reject(err);
-								})
-							} else {
-								console.log('Something really went wrong! Error: ' + JSON.stringify(err));
-								reject(err);
-							}
-						});
-					}
+								}
+							});
+						}
+					});
 				});
-			});
+			}
 		});
 	});
 }
@@ -126,10 +131,12 @@ var insertToMongo = function(db, collection, agentMongoData) {
 				console.log('Error: ' + JSON.stringify(err) + ' in file: ' + agentMongoData._id + '. Trying to replace by unicode equivalent \\uff0e');
 				//replace all keys that has . in them with its unicode equivalent
 				if (err.message.indexOf('must not contain \'.\'') != -1) {
-					var propertiesKeys = Object.keys(agentMongoData.properties);
-					for (var count = 0; count < propertiesKeys.length; count++) {
-						var dotReplacedKey = propertiesKeys[count].replace('.', '\\uff0e');
-						agentMongoData = JSON.parse(JSON.stringify(agentMongoData).replace(propertiesKeys[count], dotReplacedKey));
+					var agentMongoProperties = agentMongoData["properties"];
+					var propertyKeys = [];
+					traverse(agentMongoProperties, propertyKeys);
+					for (var count = 0; count < propertyKeys.length; count++) {
+						var dotReplacedKey = propertyKeys[count].replace('.', '\\uff0e');
+						agentMongoData["properties"] = JSON.parse(JSON.stringify(agentMongoData["properties"]).replace(propertyKeys[count], dotReplacedKey));
 					}
 					reject(agentMongoData);
 				} else {
@@ -140,4 +147,21 @@ var insertToMongo = function(db, collection, agentMongoData) {
 			}
 		});
 	});
+}
+
+var traverse = function(jsonObj, keys) {
+	if (jsonObj && typeof jsonObj == "object") {
+		var jsonKeys = Object.keys(jsonObj);
+		jsonKeys.forEach(function(key) {
+			if (key) {
+				// k is either an array index or object key
+				keys.push(key);
+				var traversedKeys = traverse(jsonObj[key], keys);
+				if (traversedKeys && traversedKeys.length != 0) {
+					keys.push(traversedKeys);
+					return;
+				}
+			}
+		});
+	}
 }
